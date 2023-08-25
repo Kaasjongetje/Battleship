1a210fc1b07ae85b6a2440abe23d3ab2a89ebf05
Kaasjongetje <cas.hukkelhoven@gmail.com>
2023-08-25 09:33:15 +0200

    Simpele tests voor ship en board

---
+++ tests/board.test.js
@@ -0,0 +1,17 @@
+const board = require('../board.js');
+
+test.only('Je kunt horizontaal plaatsen', () => {
+    expect(3).toBe(3);
+});
+
+test('Je kunt niet buiten de map horizontaal plaatsen', () => {
+
+});
+
+test('Je kunt verticaal plaatsen', () => {
+    
+});
+
+test('Je kunt niet buiten de map verticaal plaatsen', () => {
+    
+});
---
+++ tests/ship.test.js
@@ -0,0 +1,15 @@
+import Ship from '../ship.js';
+
+test('Je kunt een ship hitten', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+});
+
+test('Een ship kan zinken', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+});
