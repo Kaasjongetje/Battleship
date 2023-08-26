4ee84bd8bd9c4015e4f8aa47cabd463e9aed6003
Kaasjongetje <cas.hukkelhoven@gmail.com>
2023-08-26 10:35:28 +0200

    Method gemaakt die kijkt of een locatie binnen de map is

--- board.js
+++ board.js
@@ -1,4 +1,5 @@
 import Tile from "./tile.js";
+import Ship from "./ship.js";
 
 export default class Board {
     static size = 10;
@@ -14,6 +15,29 @@ export default class Board {
     }
 
     canPlace (ship, location) {
-
+        return this.isSuitableLocation(ship, location, (tile) => !tile.isOccupied());
     }
+
+    isSuitableLocation (ship, location, isDesirable) {
+        const locations = ship.getLocations(location);
+
+        for (const location of locations) {
+            if (!Board.isValidLocation(location)) {
+                return false;
+            }
+
+            const tile = this.tiles[location[0]][location[1]];
+
+            if (!isDesirable(tile)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static isValidLocation (location) {
+        return 0 <= location[0] && location[0] < Board.size && 0 <= location[1] && location[1] < Board.size;
+    }
+
 }
