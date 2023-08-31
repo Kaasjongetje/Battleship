89df16d6b8faf157bee65f44c22cec69955ecc72
Kaasjongetje <cas.hukkelhoven@gmail.com>
2023-08-31 08:47:06 +0200

    Functie die potentiele schepen returnt gemaakt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Ship from "./ship.js";
 import Board from "./board.js";
 import Player from "./player.js";
+import AI from "./ai.js";
 
 // Naam krijgen van form
 const player = new Player("Kaas"); 
@@ -8,3 +9,4 @@ const computer = new Player("The Computer");
 
 let mode = 'preparation';
 let current = player;
+
