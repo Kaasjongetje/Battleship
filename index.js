e3e601fb5e7c7d245abc7d56540885bec339c353
Kaasjongetje <cas.hukkelhoven@gmail.com>
2023-09-05 12:09:01 +0200

    Je kunt via de form naar de voorbereiding gaan

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ import Ship from "./ship.js";
 import Board from "./board.js";
 import Player from "./player.js";
 import AI from "./ai.js";
-import { getForm } from "./dom.js";
+import { loadPage, getForm, getPreparation } from "./dom.js";
+import { validateInput } from "./script.js";
 
+let player;
+const computer = new Player('The Computer');
 
 loadPage(getForm());
 
-
-
-
-
-
-
-
-
-
-function loadPage (element) {
-    removeChildNodes(document.body);
-    document.body.appendChild(element);
+export function initializePlayer (name) {
+    player = new Player(name);
+    loadPage(getPreparation());
 }
 
-function removeChildNodes (element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
