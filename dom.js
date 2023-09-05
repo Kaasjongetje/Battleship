999ef1ff1423accaaa80c5bacef2f96495516fac
Kaasjongetje <cas.hukkelhoven@gmail.com>
2023-09-05 12:08:02 +0200

    Elements hebben event listeners gekregen

--- dom.js
+++ dom.js
@@ -1,21 +1,80 @@
+import { validateForm } from "./script.js";
+import { validateInput } from "./script.js";
+import Ship from "./ship.js";
+
+export function loadPage (element) {
+    removeChildNodes(document.body);
+    document.body.appendChild(element);
+}
+
 export function getForm() {
-    const formElement = document.createElement("form");
-    
-    const nameLabel = document.createElement("label");
-    nameLabel.setAttribute("for", "name");
-    nameLabel.textContent = "Name";
-    
-    const inputElement = document.createElement("input");
-    inputElement.setAttribute("type", "text");
-    inputElement.setAttribute("id", "name");
-    
-    const buttonElement = document.createElement("button");
-    buttonElement.setAttribute("type", "submit");
-    buttonElement.textContent = "Continue";
-    
-    formElement.appendChild(nameLabel);
-    formElement.appendChild(inputElement);
-    formElement.appendChild(buttonElement);
-    
-    return formElement;
+    const form = document.createElement("form");
+    form.setAttribute("novalidate", "");
+    form.setAttribute("id", "name-form");
+
+    const label = document.createElement("label");
+    label.setAttribute("for", "name");
+    label.textContent = "Name";
+    form.appendChild(label);
+
+    const inputWrapper = document.createElement("div");
+    inputWrapper.className = "input-wrapper";
+    form.appendChild(inputWrapper);
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "name");
+    input.setAttribute("minlength", "1");
+    input.setAttribute("maxlength", "15");
+    input.setAttribute("required", "required");
+    inputWrapper.appendChild(input);
+
+    const errorMessage = document.createElement("div");
+    errorMessage.className = "error-message";
+    errorMessage.setAttribute("id", "name-error");
+    inputWrapper.appendChild(errorMessage);
+
+    const submitButton = document.createElement("button");
+    submitButton.setAttribute("type", "submit");
+    submitButton.textContent = "Continue";
+    form.appendChild(submitButton);
+
+    input.addEventListener('input', () => validateInput(input, errorMessage));
+    form.addEventListener('submit', validateForm);
+
+    return form;
+}
+
+export function getPreparation() {
+    const preparationDiv = document.createElement('div');
+    preparationDiv.classList.add('preparation');
+
+    const boardDiv = document.createElement('div');
+    boardDiv.classList.add('board');
+
+    for (let i = 0; i < 100; i++) {
+        const tileDiv = document.createElement('div');
+        tileDiv.classList.add('tile');
+        boardDiv.appendChild(tileDiv);
+    }
+    preparationDiv.appendChild(boardDiv);
+
+    const shipContainerDiv = document.createElement('div');
+    shipContainerDiv.classList.add('ship-container');
+
+    for (const shipType in Ship.SHIPS) {
+        const shipDiv = document.createElement('div');
+        shipDiv.classList.add('ship', shipType.toLowerCase());
+        shipDiv.setAttribute('draggable', 'true');
+        shipContainerDiv.appendChild(shipDiv);
+    }
+    preparationDiv.appendChild(shipContainerDiv);
+
+    return preparationDiv;
+}
+
+function removeChildNodes (element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
 }
