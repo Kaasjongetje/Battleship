055e2afb11b1d10706868019718b88e5cca80b63
Kaasjongetje <cas.hukkelhoven@gmail.com>
2023-09-08 14:20:44 +0200

    For each tile functie gemaakt voor double arrays

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import Ship from "./ship.js";
 import Board from "./board.js";
 import Player from "./player.js";
-import AI from "./ai.js";
+import AI, { printArray } from "./ai.js";
 import { loadPage, getForm, getPreparation } from "./dom.js";
 import { validateInput } from "./script.js";
 
-let player;
-const computer = new Player('The Computer');
+const board = new Board();
+const ai = new AI(board);
 
-loadPage(getForm());
+board.ships[0].hits = 999;
+ai.updateProbabilityMap();
+
+printArray(ai.probabilityMap);
+
+console.log(ai.getHighestProbabilityLocations());
+
+// let player = new Player('Kaasjongetje');// let player;
+// const computer = new Player('The Computer');
+
+// loadPage(getPreparation());// loadPage(getForm());
 
 export function initializePlayer (name) {
     player = new Player(name);
--- tests/board/foreachtile.test.js
+++ tests/board/foreachtile.test.js
@@ -2,26 +2,26 @@ import Board from "../../board.js";
 
 test('Hij loopt over het hele bord', () => {
     const board = new Board();
-    board.forEachTile((tile) => tile.attacked = true);
+    Board.forEachTile(board.tiles, (tile) => tile.attacked = true);
     expect(board.tiles[4][5].attacked).toBe(true);
     expect(board.tiles[2][1].attacked).toBe(true);
     expect(board.tiles[7][8].attacked).toBe(true);
 });
 
-test.skip('Hij loopt het aangegeven gebied', () => {
+test('Hij loopt het aangegeven gebied', () => {
     const board = new Board();
     const callback = (tile) => tile.attacked = true;
-    board.forEachTile(callback, [0, 0], [9, 7]);
+    Board.forEachTile(board.tiles, callback, [0, 0], [9, 7]);
     // Binnen de range
     expect(board.tiles[4][7].attacked).toBe(true);
     // Buiten de range
     expect(board.tiles[4][8].attacked).toBe(false);
 });
 
-test.skip('Hij returnt row en cell getallen', () => {
+test('Hij returnt row en cell getallen', () => {
     const board = new Board();
     let number = 0;
     const callback = (tile, row, cell) => number = row + cell;
-    board.forEachTile(callback, [1, 1], [1, 1]);
+    Board.forEachTile(board.tiles, callback, [1, 1], [1, 1]);
     expect(number).toEqual(2);
 });
