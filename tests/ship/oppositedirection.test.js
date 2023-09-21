ab6b9fae550f93c47eae75623eebea0b9afe510c
Kaasjongetje <cas.hukkelhoven@gmail.com>
2023-09-21 12:16:41 +0200

    De functie weer weggehaald, veel te complex zo

--- tests/ship/oppositedirection.test.js
+++
@@ -1,9 +0,0 @@
-import Ship from "../../ship.js";
-
-test('Returnt tegenovergestelde horizontale richting', () => {
-    expect(Ship.getOppositeDirection('horizontal')).toEqual([0, -1]);
-});
-
-test('Returnt tegenovergestelde verticale richting', () => {
-    expect(Ship.getOppositeDirection('vertical')).toEqual([-1, 0]);
-});
